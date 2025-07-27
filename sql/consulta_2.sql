6ef2b704d76c47a3dd7c3ef9c46965bb8907e72a
TalesHonorio <honoriotales@gmail.com>
2025-07-27 15:28:20 -0300

    Atualiza no código com a criacao das consultas 2 e 3 em SQL, quais sejam analise de engajamentos e comentarios, respectivamete

--- sql/consulta_2.sql
+++ sql/consulta_2.sql
@@ -1,2 +1,12 @@
 -- Plataforma com maior engajamento (total de interações like, share, comment)
 
+SELECT 
+    p.nome_plataforma,
+    COUNT(*) AS total_engajamento
+FROM interacao i
+JOIN tipointeracao t ON i.id_tipo_interacao = t.id_tipo_interacao
+JOIN plataforma p ON i.id_plataforma = p.id_plataforma
+WHERE t.nome_tipo_interacao IN ('like', 'share', 'comment')
+GROUP BY p.nome_plataforma
+ORDER BY total_engajamento DESC
+LIMIT 1;
